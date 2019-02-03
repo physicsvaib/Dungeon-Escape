00662416a4efa013b5b3805cd05dedcd2cc2708b
physicsvaib <physicsvaib@gmail.com>
2019-02-03 22:46:41 +0530

    Till 6

---
+++ Assets/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int spped;
+    [SerializeField] protected int gems;
+
+    [SerializeField] protected Transform PointA, PointB;
+    Vector3 currentPos;
+    Animator anim;
+    SpriteRenderer sprite;
+    [SerializeField] bool isIdle;
+
+    public void initi()
+    {
+        anim = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        Update();
+        isIdle = false;
+    }
+
+    private void Start()
+    {
+        initi();
+    }
+
+    void Move()
+    {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+
+
+            if (currentPos == PointA.position)
+            {
+                sprite.flipX = false;
+                Debug.Log("False " + this.gameObject.name);
+            }
+            else
+            {
+                sprite.flipX = true;
+                Debug.Log("True " + this.gameObject.name);
+
+            }
+            if (transform.position == PointA.position)
+            {
+                currentPos = PointB.position;
+                StartCoroutine("Inverter");
+
+            }
+            else if (transform.position == PointB.position)
+            {
+                currentPos = PointA.position;
+
+                StartCoroutine("Inverter");
+
+            }
+            transform.position = Vector3.MoveTowards(transform.position, currentPos, spped * Time.deltaTime);
+
+        }
+        else
+        {
+            isIdle = !isIdle;
+        }
+    }
+    IEnumerator Inverter()
+    {
+
+        isIdle = !isIdle;
+        yield return new WaitForSeconds(3f);
+
+    }
+
+    void animate()
+    {
+        if (isIdle == true)
+        {
+            anim.SetBool("Idle", true);
+        }
+        else
+        {
+            anim.SetBool("Idle", false);
+        }
+    }
+
+    public virtual void Update() {
+
+        Move();
+        animate();
+
+    }
+
+
+}
---
+++ Assets/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator[] anim ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentsInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    public void run(float move)
+    {
+        anim[0].SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void jump(bool jump)
+    {
+        anim[0].SetBool("Jump", jump);
+    }
+
+    public void attack()
+    {
+        anim[0].SetTrigger("Attack");
+        anim[1].SetTrigger("attack");
+    }
+
+    public void JumpAndAttack(bool jaa)
+    {
+        anim[0].SetBool("JumpAndAttack", jaa);
+    }
+
+}
